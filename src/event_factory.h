15028d2b98cf93e2553a0da446f48b077b26e2c6
JD <jeandaniel.michaud@ge.com>
2015-09-04 08:10:22 +0200

    Add missing event_factory

---
+++ src/event_factory.h
@@ -0,0 +1,27 @@
+/*! \brief User inputs
+ *
+ * The Input class represent the possible user inputs.
+ */
+
+#ifndef __EVENT_FACTORY_H__
+#define __EVENT_FACTORY_H__
+
+#include "event.h"
+#include "input.h"
+
+class EventFactory {
+public:
+  Event&& build_event(uint event_id) {
+    if (isprint(event_id))             return std::move(Printable(event_id));
+    else if (isarrow(event_id))        return std::move(Arrow(event_id));
+    else if (isnav(event_id))          return std::move(Nav(event_id));
+    else if (isfunctionkey(event_id))  return std::move(Function(event_id));
+    else if (isctrl(event_id))         return std::move(Ctrl(event_id));
+    else if (isredraw(event_id))       return std::move(Redraw(event_id));
+
+    LOGERR("EventFactory: unknown event_id " << event_id);
+    throw std::runtime_error("unknown event id " + std::to_string(event_id));
+  }
+};
+
+#endif //__EVENT_FACTORY_H__
